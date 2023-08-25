1b0f7e3f12d4257d6a2c13c2a3dc22021447aa63
bonj4 <omerfarukkaraboga@gmail.com>
2023-08-25 12:56:34 +0300

    fixed normalize fault

--- dataset.py
+++ dataset.py
@@ -4,52 +4,80 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import cv2
 from config import *
-
 class Dataset(Dataset):
-    def __init__(self, image_dir, label_dir, transform=None):
-
+    def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
-        self.label_dir = label_dir
+        self.mask_dir = mask_dir
         self.transform = transform
-        self.annotations = os.listdir(image_dir)
+        self.images = os.listdir(image_dir)
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.images)
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.annotations[index])
-        image = np.array(cv2.imread(img_path))
-        label_path = os.path.join(self.label_dir, self.annotations[index][:-5] + ".txt")
-        mask =  self._convert_polygon_to_mask(file_path=label_path,image=image)
-        # mask[mask == 255] = 1.0
+        img_path = os.path.join(self.image_dir, self.images[index])
+        image = np.array(Image.open(img_path).convert("RGB"),dtype=np.float32)
+
+        try:
+            mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", ".png"))
+            mask = np.array(Image.open(mask_path).convert("L"),dtype=np.float32)
+
+        except:
+            mask = np.zeros(image.shape[:2])
+        mask/= 255.0
+        image/= 255.0
 
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)
             image = augmentations["image"]
             mask = augmentations["mask"]
 
-        return image,mask
-    def _convert_polygon_to_mask(self,file_path,image):
-        mask = np.zeros(image.shape[:2], np.uint8)
-        # Step 1: Read the text file line by line
-        with open(file_path, 'r') as file:
-            for line in file:
-                array = []
-                elements = line.strip().split()[1]
-                elements=elements.split(",")
-                for idx in range(0, len(elements), 2):
-                    array.append([int(float(elements[idx])), int(float(elements[idx + 1]))])
-                # print(array)
-                array=np.array(array)
-                mask = cv2.drawContours(mask, [array], -1, (255), -1, cv2.LINE_AA)
-        return mask
+        return image, mask
+# class Dataset(Dataset):
+#     def __init__(self, image_dir, label_dir, transform=None):
+#
+#         self.image_dir = image_dir
+#         self.label_dir = label_dir
+#         self.transform = transform
+#         self.annotations = os.listdir(image_dir)
+#
+#     def __len__(self):
+#         return len(self.annotations)
+#
+#     def __getitem__(self, index):
+#         img_path = os.path.join(self.image_dir, self.annotations[index])
+#         image = np.array(cv2.imread(img_path))
+#         label_path = os.path.join(self.label_dir, self.annotations[index][:-5] + ".txt")
+#         mask =  self._convert_polygon_to_mask(file_path=label_path,image=image)
+#         # mask[mask == 255] = 1.0
+#
+#         if self.transform is not None:
+#             augmentations = self.transform(image=image, mask=mask)
+#             image = augmentations["image"]
+#             mask = augmentations["mask"]
+#
+#         return image,mask
+#     def _convert_polygon_to_mask(self,file_path,image):
+#         mask = np.zeros(image.shape[:2], np.uint8)
+#         # Step 1: Read the text file line by line
+#         with open(file_path, 'r') as file:
+#             for line in file:
+#                 array = []
+#                 elements = line.strip().split()[1]
+#                 elements=elements.split(",")
+#                 for idx in range(0, len(elements), 2):
+#                     array.append([int(float(elements[idx])), int(float(elements[idx + 1]))])
+#                 # print(array)
+#                 array=np.array(array)
+#                 mask = cv2.drawContours(mask, [array], -1, (255), -1, cv2.LINE_AA)
+#         return mask
 
 def test():
 
     transform = val_transforms
-
-    dataset = Dataset(        "/home/visio-ai/PycharmProjects/FaceAndPlate/Dataset/images",
-        "/home/visio-ai/PycharmProjects/FaceAndPlate/Dataset/labels",
+    dataset = Dataset(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
         transform=transform,
     )
     loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
